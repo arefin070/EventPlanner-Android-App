8a9f3d34ccb089ac841e474a48f9b85519efea3e
arefin070 <arefinislam070@gmail.com>
2019-01-12 03:22:25 +0600

    LogIn and SignUp

--- app/src/main/java/bd/ac/uiu/eventplanner/SignupActivity.java
+++ app/src/main/java/bd/ac/uiu/eventplanner/SignupActivity.java
@@ -2,16 +2,12 @@ package bd.ac.uiu.eventplanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
 
-public class Home_Activity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_);
-
-
+        setContentView(R.layout.activity_signup);
     }
 }
